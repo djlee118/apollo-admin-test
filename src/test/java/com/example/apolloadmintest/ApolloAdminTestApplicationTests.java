c1b0c8e15a11d33c08c5103c2c6bbf966a5db46f
djlee118 <djlee118@nate.com>
2022-02-04 13:27:40 +0900

    Initial commit

---
+++ src/test/java/com/example/apolloadmintest/ApolloAdminTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apolloadmintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloAdminTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
